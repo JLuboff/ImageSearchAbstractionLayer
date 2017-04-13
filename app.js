b7898b5c0c653a666c6cb7ad7fc1c0eb34c20653
Jason Luboff <jluboff@gmail.com>
2017-04-13 23:55:11 +0000

    Recents work in progress

--- app.js
+++ app.js
@@ -3,16 +3,24 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const mongo = require('mongodb').MongoClient;
+const moment = require("moment");
 const imgSearch = require('node-google-image-search');
 const port = process.env.PORT || 8080;
 
 var app = express();
 
-app.get("/:query", function(req, res){
+app.get("/api/:query", function(req, res){
+     mongo.connect("mongodb://" + process.env.MONGO_USER + ":" + process.env.MONGO_PASS +"@ds161190.mlab.com:61190/imgsrch", function(err, db){
+         if(err) console.log(err);
+         
     let query = req.params.query;
     let offset = req.query.offset === undefined ? 0 : req.query.offset;
-    console.log(query, offset);
-    
+    let time = moment().format();
+    let search = {
+        term: query,
+        when: time
+    }
+    console.log(search);
     var results = imgSearch(query, callback, offset , 10);
 
     function callback(results) {
@@ -25,9 +33,26 @@ app.get("/:query", function(req, res){
         obj["context"] = el.image.contextLink;
         arr.push(obj);
     }); 
-    
     res.send(arr);
+    db.collection("recent").insert(search, function(err, data){
+        if(err) throw err;
+        db.close();
+    });
 }
-
 });
+});
+
+app.get("/recent", function(req, res){
+    mongo.connect("mongodb://" + process.env.MONGO_USER + ":" + process.env.MONGO_PASS +"@ds161190.mlab.com:61190/imgsrch", function(err, db){
+         if(err) console.log(err);
+       db.collection("recent").find({}).toArray(function(err, data){
+           if (err) throw err;
+           console.log(data);
+           res.send(data);
+           db.close();
+       });
+       
+      
+    })
+})
 app.listen(port);
