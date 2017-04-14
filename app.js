fa256ff3f6057667274409dd1e41096d472453b3
Jason Luboff <jluboff@gmail.com>
2017-04-14 19:23:10 +0000

    All user stories completed

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//google api key: AIzaSyCkAzR6ixhqaGJLhoQXDmJ7zS67cTSD9Fk 
+
 "use strict";
 const express = require('express');
 const dotenv = require('dotenv').config();
@@ -10,49 +10,48 @@ const port = process.env.PORT || 8080;
 var app = express();
 
 app.get("/api/:query", function(req, res){
-     mongo.connect("mongodb://" + process.env.MONGO_USER + ":" + process.env.MONGO_PASS +"@ds161190.mlab.com:61190/imgsrch", function(err, db){
-         if(err) console.log(err);
-         
+  mongo.connect("mongodb://" + process.env.MONGO_USER + ":" + process.env.MONGO_PASS +"@ds161190.mlab.com:61190/imgsrch", function(err, db){
+    if(err) console.log(err);
+    
     let query = req.params.query;
     let offset = req.query.offset === undefined ? 0 : req.query.offset;
     let time = moment().format();
     let search = {
-        term: query,
-        when: time
-    }
-    console.log(search);
+      term: query,
+      when: time
+    };
+    
     var results = imgSearch(query, callback, offset , 10);
-
+    
     function callback(results) {
-        let arr = [];
-        results.forEach(function(el){
+      let arr = [];
+      results.forEach(function(el){
         let obj = {};
         obj["url"] = el.link;
         obj["snippet"] = el.snippet;
         obj["thumbnail"] = el.image.thumbnailLink;
         obj["context"] = el.image.contextLink;
         arr.push(obj);
-    }); 
-    res.send(arr);
-    db.collection("recent").insert(search, function(err, data){
+      });
+      res.send(arr);
+      db.collection("recent").insert(search, function(err, data){
         if(err) throw err;
         db.close();
-    });
-}
-});
+      });
+    }
+  });
 });
 
 app.get("/recent", function(req, res){
-    mongo.connect("mongodb://" + process.env.MONGO_USER + ":" + process.env.MONGO_PASS +"@ds161190.mlab.com:61190/imgsrch", function(err, db){
-         if(err) console.log(err);
-       db.collection("recent").find({}).toArray(function(err, data){
-           if (err) throw err;
-           console.log(data);
-           res.send(data);
-           db.close();
-       });
-       
-      
-    })
+  mongo.connect("mongodb://" + process.env.MONGO_USER + ":" + process.env.MONGO_PASS +"@ds161190.mlab.com:61190/imgsrch", function(err, db){
+    if(err) console.log(err);
+    db.collection("recent").find({}, {_id: 0, term: 1, when: 1}).sort({when: -1}).limit(10).toArray(function(err, data){
+      if (err) throw err;
+      res.send(data);
+      db.close();
+    });
+    
+    
+  })
 })
 app.listen(port);
